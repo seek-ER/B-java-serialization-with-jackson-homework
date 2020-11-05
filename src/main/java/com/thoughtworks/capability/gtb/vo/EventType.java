6ac5ce60b16152905bb78900bf080f6bdedaf671
Anson <1072476988@qq.com>
2020-11-05 13:48:46 +0800

    [Lingzhen Kong] feat: get eventType code.

--- src/main/java/com/thoughtworks/capability/gtb/vo/EventType.java
+++ src/main/java/com/thoughtworks/capability/gtb/vo/EventType.java
@@ -1,11 +1,18 @@
 package com.thoughtworks.capability.gtb.vo;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum EventType {
   UPLOAD("U"), DOWNLOAD("D");
 
-  private String code;
+  private final String code;
 
   EventType(String code) {
     this.code = code;
+  }
+
+  @JsonValue
+  public String getValue() {
+    return code;
   }
 }
