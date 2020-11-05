f7128f015f6721a8dc1e9d244f80e77507b6748f
Anson <1072476988@qq.com>
2020-11-05 13:47:57 +0800

    [Lingzhen Kong] feat: unwrap user.

--- src/main/java/com/thoughtworks/capability/gtb/vo/EventVo.java
+++ src/main/java/com/thoughtworks/capability/gtb/vo/EventVo.java
@@ -1,9 +1,13 @@
 package com.thoughtworks.capability.gtb.vo;
 
-import java.util.Date;
+import com.fasterxml.jackson.annotation.JsonUnwrapped;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.thoughtworks.capability.gtb.util.Date2LongSerializer;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.Date;
 
 @Data
 @NoArgsConstructor
@@ -13,6 +17,8 @@ public class EventVo {
   private String id;
   private String name;
   private EventType type;
+  @JsonSerialize(using = Date2LongSerializer.class)
   private Date time;
+  @JsonUnwrapped
   private UserVo user;
 }
